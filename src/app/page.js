c0989938485cca6e69274908fee0894422d12b19
Reina R <rrodriguez.ut@gmail.com>
2024-05-15 22:06:58 -0700

    update image

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
           <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
             <img
-              className="absolute inset-0 h-full w-full bg-purple-800  object-cover"
+              className="absolute inset-0 h-full w-full bg-purple-800 p-10  object-cover"
               src="https://i.imgur.com/4AMWK7G.jpg"
               alt=""
             />
